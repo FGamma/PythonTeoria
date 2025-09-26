e6a7388be14ca20abf47f553dd4bee7721f7b90b
FGamma <francesco.gammaraccio@gmail.com>
2025-09-26 13:12:35 +0200

    Versione in italiano

--- OOP/class_attribute_rabbit.py
+++ OOP/class_attribute_rabbit.py
@@ -1,20 +1,20 @@
-class Rabbit:
+class Coniglio:
     # Attributo di classe definito fuori __init__
     tag = 1
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.rid = Rabbit.tag
-        Rabbit.tag +=1
+    def __init__(self, nome, eta):
+        self.nome = nome
+        self.eta = eta
+        self.rid = Coniglio.tag
+        Coniglio.tag +=1
 
     def __str__(self):
-        return (f"Il coniglio di nome {self.name} ha il tag univoco"
+        return (f"Il coniglio di nome {self.nome} ha il tag univoco"
                 f" {self.rid}")
 
 # Ogni volta che creo una nuova instanza, il tag aumenta il suo valore
-con_1 = Rabbit("Rabbit1",2)
-con_2 = Rabbit("Rabbit2",1)
-con_3 = Rabbit("Rabbit3",3)
+con_1 = Coniglio("Tim",2)
+con_2 = Coniglio("Tom",1)
+con_3 = Coniglio("Tam",3)
 
 print(con_1)
 print(con_2)
